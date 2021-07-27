c7c63e9ac924755a02f2fffcf06bf3f34ad47fda
Daniel Jackson <daniel@snilldesign.no>
2021-07-27 17:04:36 +0200

    Initialize repo

---
+++ lib/danieljackson_learning_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "danieljackson_learning_palindrome/version"
+
+module DanieljacksonLearningPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "danieljackson_learning_palindrome"
+
+require "minitest/autorun"
